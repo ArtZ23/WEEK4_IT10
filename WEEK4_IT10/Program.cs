44eb041c6a540133651248bdc7da77219afce201
ArtZ23 <zzartur77@gmail.com>
2025-08-21 21:41:26 +0400

    change color

--- WEEK4_IT10/Program.cs
+++ WEEK4_IT10/Program.cs
@@ -2,10 +2,13 @@
 
 Console.WriteLine("Hello World!");
 Console.WriteLine("Hello C#");
-
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.Red;
 
 Console.WriteLine("    / |");
 Console.WriteLine("   /  |");
 Console.WriteLine("  /   |");
 Console.WriteLine(" /    |");
 Console.WriteLine("/_ _ _|");
+
+Console.Title = "My C# Program";
