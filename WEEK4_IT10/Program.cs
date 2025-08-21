7d63442eff6106cef312999dcaf3c01ad4aea794
ArtZ23 <zzartur77@gmail.com>
2025-08-21 21:30:33 +0400

    Add new changes

---
+++ WEEK4_IT10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WEEK4_IT10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        } 
+
+
+    }
+}
