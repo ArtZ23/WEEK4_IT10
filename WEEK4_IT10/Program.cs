dff3f0508e9221b6ef0be99ea0f3217c54dd6562
ArtZ23 <zzartur77@gmail.com>
2025-08-21 21:38:53 +0400

    Draw Pyramid

--- WEEK4_IT10/Program.cs
+++ WEEK4_IT10/Program.cs
@@ -2,3 +2,10 @@
 
 Console.WriteLine("Hello World!");
 Console.WriteLine("Hello C#");
+
+
+Console.WriteLine("    / |");
+Console.WriteLine("   /  |");
+Console.WriteLine("  /   |");
+Console.WriteLine(" /    |");
+Console.WriteLine("/_ _ _|");
